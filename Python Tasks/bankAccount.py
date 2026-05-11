cfae3c50621550ef38ea5e4577d28c677a2e9f91
Phoenix N <pn22178@my.westlake.school.nz>
2026-05-11 14:19:30 +1200

    bank gui pt. 3

--- Python Tasks/bankAccount.py
+++ Python Tasks/bankAccount.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class bankAccount:
     def __init__(self, name):
@@ -31,5 +32,44 @@ class bankGUI:
         self.name_entry = tk.Entry(self.input_frame)
         self.name_entry.grid(row=1, column=0)
 
-        tk.Button()
+        tk.Button(self.input_frame, text="Create Account", command=self.create_account())
+
+        self.name_label = tk.Label(self.bankDetails_frame, text="Account: -")
+        self.name_label.pack()
+
+        self.balance_label = tk.Label(self.bankDetails_frame, text="Balance: -")
+        self.balance_label.pack()
+
+        tk.Button(self.bankActions_frame, text="Deposit", command=self.account_deposit).grid(row=0, column=0)
+        tk.Button(self.bankActions_frame, text="Withdraw").grid(row=0, column=1)
+    
+    def create_account(self):
+        name = self.name_entry.get().strip()
+
+        if name == "":
+            messagebox.showerror("Account Error", "You must provide an account name to create an account")
+            return
         
+        if messagebox.askokcancel("Confirm Account", f"Are you sure you would like to create the bank account: {name}"):
+            self.account = bankAccount(name)
+        
+        return
+    
+    def funds_window(self, request):
+        if request is None:
+            return
+        
+        fundsWind = tk.Toplevel(self.root)
+        fundsWind.title(f"{request} Funds")
+        funds_infoFrame = tk.Frame(fundsWind, padx=10, pady=10)
+        funds_inputFrame = tk.Frame(fundsWind, padx=10, pady=10)
+        fundsWind.geometry("500x800")
+
+        tk.Label(funds_infoFrame, text=f"Current Balance: ${self.account.balance}")
+        tk.Label(funds_infoFrame, text=f"How much would you like to {request}")
+        
+        
+        return
+
+    def account_deposit(self):
+        return
